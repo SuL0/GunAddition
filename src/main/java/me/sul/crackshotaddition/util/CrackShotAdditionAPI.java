ad2ae0f14cdf67c63f4c42591342e43f16c8376c
SuL <jjw011202@naver.com>
2020-07-14 22:52:22 +0900

    유지보수를 위해 getWeaponAmmoAmount_CSP()를 삭제함

--- src/main/java/me/sul/crackshotaddition/util/CrackShotAdditionAPI.java
+++ src/main/java/me/sul/crackshotaddition/util/CrackShotAdditionAPI.java
@@ -17,54 +17,8 @@ public class CrackShotAdditionAPI {
 	}
 
 	public static int getWeaponAmmoAmount(Player player, String parent_node, ItemStack is) {
-		int currentAmmo = getWeaponAmmoAmount_CSP(is);
-		// 듀얼모드(|)가 있을 때
-		if (currentAmmo == -1) {
-			// NOTE: Airstrike같이 한번 쓰고 없어지는 템은 null이 뜸. (총기 내구도 때문에 파괴돼도 그럴지도?)
-			currentAmmo = CrackShotAddition.getCSDirector().getAmmoBetweenBrackets(player, parent_node, is);
-		}
-		return currentAmmo;
-	}
-
-	private static int getWeaponAmmoAmount_CSP(ItemStack is) {
-		if (checkIfItemIsGun(is)) {
-			String nbtItemName = CSMinion.getInstance().getItemNbtName(is);
-			if (!nbtItemName.contains("ᴿ")) {
-				if (!nbtItemName.contains("«")) {
-					return -1;
-				}
-
-				if (nbtItemName.contains("|")) {
-					return -1;
-				}
-
-				String[] var2 = nbtItemName.split("«");
-				String var3;
-				if (nbtItemName.contains("◀") || nbtItemName.contains("▶")) {
-					if (nbtItemName.contains("◀")) {
-						var3 = var2[1].split("◀")[0].replaceAll(" ", "");
-						if (!var3.contains("×") && !var3.contains("?")) {
-							return Integer.parseInt(var3);
-						}
-
-						return -1;
-					}
-
-					if (nbtItemName.contains("▶")) {
-						var3 = var2[1].split("▶")[1].replaceAll(" ", "").split("»")[0];
-						if (!var3.contains("×") && !var3.contains("?")) {
-							return Integer.parseInt(var3);
-						}
-
-						return -1;
-					}
-				}
-
-				var3 = var2[1].split("»")[0];
-				return Integer.parseInt(var3);
-			}
-		}
-		return -1;
+		// TODO: Airstrike같이 한번 쓰고 없어지는 템은 null이 뜸. (총기 내구도 때문에 파괴돼도 그럴지도?)
+		return CrackShotAddition.getCSDirector().getAmmoBetweenBrackets(player, parent_node, is);
 	}
 
 	public static String getINameFromConfig(String parentNode) {
