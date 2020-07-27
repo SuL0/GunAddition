66f81e29716cd4e667af611d3b60e5bd2bcfe0db
SuL <jjw011202@naver.com>
2020-07-28 04:38:02 +0900

    코드 정리

--- src/main/java/me/sul/crackshotaddition/WeaponItemFlutterFixation.java
+++ src/main/java/me/sul/crackshotaddition/WeaponItemFlutterFixation.java
@@ -2,32 +2,32 @@ package me.sul.crackshotaddition;
 
 import me.sul.crackshotaddition.util.CrackShotAdditionAPI;
 import me.sul.servercore.playertoolchangeevent.PlayerMainItemChangedConsideringUidEvent;
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 public class WeaponItemFlutterFixation implements Listener {
+    private final PotionEffectType POTION_EFFECT_TYPE = PotionEffectType.SLOW_DIGGING;
+    
     // 서버에 들어왔을 때도 이 이벤트가 실행되기때문에 onJoin은 넣을 필요가 없음.
     @EventHandler
     public void onMainItemChanged(PlayerMainItemChangedConsideringUidEvent e) {
         if (CrackShotAdditionAPI.isValidCrackShotWeapon(e.getNewItemStack())) {
-            e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 1000000, 256, false, false), true);
+            e.getPlayer().addPotionEffect(new PotionEffect(POTION_EFFECT_TYPE, 1000000, 256, false, false), true);
             return;
         }
-        if (e.getPlayer().hasPotionEffect(PotionEffectType.SLOW_DIGGING)) {
-            e.getPlayer().removePotionEffect(PotionEffectType.SLOW_DIGGING);
+        if (e.getPlayer().hasPotionEffect(POTION_EFFECT_TYPE)) {
+            e.getPlayer().removePotionEffect(POTION_EFFECT_TYPE);
         }
 
     }
 
     @EventHandler
     public void onQuit(PlayerQuitEvent e) {
-        if (e.getPlayer().hasPotionEffect(PotionEffectType.SLOW_DIGGING)) {
-            e.getPlayer().removePotionEffect(PotionEffectType.SLOW_DIGGING);
+        if (e.getPlayer().hasPotionEffect(POTION_EFFECT_TYPE)) {
+            e.getPlayer().removePotionEffect(POTION_EFFECT_TYPE);
         }
     }
 }
