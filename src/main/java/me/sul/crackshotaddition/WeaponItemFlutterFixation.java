ac2db9e09973c71a1446721246ac0b47aecfdbe8
SuL <jjw011202@naver.com>
2020-07-27 19:49:53 +0900

    서버에 들어왔을 때 손에 아이템이 있다면 onMainItemChanged()가 실행되기 때문에 onJoin()은 필요가 없음

--- src/main/java/me/sul/crackshotaddition/WeaponItemFlutterFixation.java
+++ src/main/java/me/sul/crackshotaddition/WeaponItemFlutterFixation.java
@@ -11,7 +11,7 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 public class WeaponItemFlutterFixation implements Listener {
-    // 서버에 들어왔을 때도 이 이벤트가 실행되기때문에 onJoin은 넣을 필요가 없음. -> 아님. 서버 닫고 처음 들어왔을때는 그렇지만, 다시 들어오면 이벤트가 실행이 안됨.
+    // 서버에 들어왔을 때도 이 이벤트가 실행되기때문에 onJoin은 넣을 필요가 없음.
     @EventHandler
     public void onMainItemChanged(PlayerMainItemChangedConsideringUidEvent e) {
         if (CrackShotAdditionAPI.isValidCrackShotWeapon(e.getNewItemStack())) {
@@ -22,13 +22,6 @@ public class WeaponItemFlutterFixation implements Listener {
             e.getPlayer().removePotionEffect(PotionEffectType.SLOW_DIGGING);
         }
 
-    }
-
-    @EventHandler
-    public void onJoin(PlayerJoinEvent e) {
-        if (CrackShotAdditionAPI.isValidCrackShotWeapon(e.getPlayer().getInventory().getItemInMainHand())) {
-            e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 1000000, 256, false, false), true);
-        }
     }
 
     @EventHandler
