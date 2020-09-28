f958514dff2290334d7d91ae3c344933fd78956a
SuL <jjw011202@naver.com>
2020-09-29 01:02:14 +0900

    Kotlin STD Lib을 외부로 분리

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,13 +14,13 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+    compileOnly(files("C:/Users/PHR/Desktop/PluginStorage/KotlinLoader_S-1.4.10.jar"))
     compileOnly("com.destroystokyo.paper", "paper-api", "1.12.2-R0.1-SNAPSHOT")
     compileOnly("org.spigotmc", "spigot", "1.12.2-R0.1-SNAPSHOT")
     compileOnly("com.comphenix.protocol", "ProtocolLib", "4.5.1")
     compileOnly(files("C:/Users/PHR/Desktop/PluginStorage/CrackShot_SuL.jar"))
     compileOnly(files("C:/Users/PHR/Desktop/PluginStorage/CustomEntity_SuL.jar"))
-    compileOnly(files("C:/Users/PHR/Desktop/PluginStorage/NotFat/ServerCore_S-NotFat.jar"))
+    compileOnly(files("C:/Users/PHR/Desktop/PluginStorage/ServerCore_S.jar"))
     compileOnly(files("C:/Users/PHR/Desktop/PluginStorage/Dependencies/item-nbt-api-plugin-2.5.0.jar"))
 }
 
@@ -40,20 +40,12 @@ spigot {
 
 tasks.compileJava.get().options.encoding = "UTF-8"
 
-val fatJar = task("fatJar", type = Jar::class) {
-    archiveFileName.set("${project.name}_S.jar")
-    destinationDirectory.set(file("C:/Users/PHR/Desktop/PluginStorage"))
-    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
-    with(tasks.jar.get() as CopySpec)
-}
-
 tasks {
     compileKotlin.get().kotlinOptions.jvmTarget = "1.8"
     compileTestKotlin.get().kotlinOptions.jvmTarget = "1.8"
 
     jar {
-        dependsOn(fatJar)
-        archiveFileName.set("${project.name}_S-NotFat.jar")
-        destinationDirectory.set(file("C:/Users/PHR/Desktop/PluginStorage/NotFat"))
+        archiveFileName.set("${project.name}_S.jar")
+        destinationDirectory.set(file("C:/Users/PHR/Desktop/PluginStorage"))
     }
 }
