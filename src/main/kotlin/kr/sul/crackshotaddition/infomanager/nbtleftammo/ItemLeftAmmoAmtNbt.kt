9cea56a9ce5280cad67bd940f2297cd0aea2d541
SuL <jjw011202@naver.com>
2020-10-09 04:11:26 +0900

    nbtleftammo 업데이트

--- src/main/kotlin/kr/sul/crackshotaddition/infomanager/nbtleftammo/ItemLeftAmmoAmtNbt.kt
+++ src/main/kotlin/kr/sul/crackshotaddition/infomanager/nbtleftammo/ItemLeftAmmoAmtNbt.kt
@@ -6,33 +6,28 @@ import kr.sul.crackshotaddition.infomanager.extractor.WeaponInfoExtractor
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 
+
+// reloadEnabled 체크는 하면안됨(throw exception). false라면 총알수는 "null"이 아닌 "Infinity"이기 때문
 object ItemLeftAmmoAmtNbt {
     internal const val LEFTSIDE_AMMO_AMT_KEY = "csa.LeftSideAmmoAmt"
     internal const val RIGHTSIDE_AMMO_AMT_KEY = "csa.RightSideAmmoAmt"
 
-
-    fun getLeftSideAmmoAmt(p: Player, item: ItemStack): Int? {
-        val weaponInfo = WeaponInfoExtractor(p, item)
-        if (!weaponInfo.reloadEnabled) return null
-
+    // GETTER //
+    fun getLeftSideAmmoAmt(item: ItemStack): Int {
         val nbti = NBTItem(item)
-        if (!nbti.hasKey(LEFTSIDE_AMMO_AMT_KEY)) return null
+        if (!nbti.hasKey(LEFTSIDE_AMMO_AMT_KEY)) throw Exception("$item")
         return nbti.getInteger(LEFTSIDE_AMMO_AMT_KEY)
     }
-    fun getRightSideAmmoAmt(p: Player, item: ItemStack): Int? {
-        val weaponInfo = WeaponInfoExtractor(p, item)
-        if (!weaponInfo.reloadEnabled) return null
-
+    fun getRightSideAmmoAmt(item: ItemStack): Int? {
         val nbti = NBTItem(item)
         if (!nbti.hasKey(RIGHTSIDE_AMMO_AMT_KEY)) return null
         return nbti.getInteger(RIGHTSIDE_AMMO_AMT_KEY)
     }
 
 
-    fun updateLeftAmmoAmtNbt(p: Player, item: ItemStack, howToGetLeftAmmoAmt: ((WeaponInfoExtractor) -> Int)?=null) {
+    // Item NBT에 Left Ammo를 기록하기 //
+    fun updateLeftAmmoAmtNbt(p: Player, item: ItemStack) {
         val weaponInfo = WeaponInfoExtractor(p, item)
-        if (!weaponInfo.reloadEnabled) throw Exception("$p, $item, $howToGetLeftAmmoAmt")
-
         val leftSideAmmoAmt: Int
         var rightSideAmmoAmt: Int? = null
         if (weaponInfo.isDualWield() || weaponInfo.hasAttachment()) {
@@ -40,7 +35,7 @@ object ItemLeftAmmoAmtNbt {
             leftSideAmmoAmt = dualAmmo[0]
             rightSideAmmoAmt = dualAmmo[1]
         } else {
-            leftSideAmmoAmt = howToGetLeftAmmoAmt?.let { it(weaponInfo) } ?: csDirector.getAmmoBetweenBrackets(p, weaponInfo.parentNode, item)  // default way
+            leftSideAmmoAmt = csDirector.getAmmoBetweenBrackets(p, weaponInfo.parentNode, item)
         }
 
         setLeftAmmoAmtNbt(item, leftSideAmmoAmt, rightSideAmmoAmt)
@@ -48,8 +43,11 @@ object ItemLeftAmmoAmtNbt {
     private fun setLeftAmmoAmtNbt(item: ItemStack, leftAmmoAmt: Int, rightAmmoAmt: Int?) {
         val nbti = NBTItem(item)
         nbti.setInteger(LEFTSIDE_AMMO_AMT_KEY, leftAmmoAmt)
-        if (rightAmmoAmt != null)
+        if (rightAmmoAmt != null) {
             nbti.setInteger(RIGHTSIDE_AMMO_AMT_KEY, rightAmmoAmt)
+        } else if (nbti.hasKey(RIGHTSIDE_AMMO_AMT_KEY)) { // config가 바껴서 RightSideAmmo가 있다가 없어진 상황을 위함
+            nbti.removeKey(RIGHTSIDE_AMMO_AMT_KEY)
+        }
         item.itemMeta = nbti.item.itemMeta
     }
 }
--- src/main/kotlin/kr/sul/crackshotaddition/infomanager/nbtleftammo/ItemLeftAmmoAmtNbtUpdater.kt
+++ src/main/kotlin/kr/sul/crackshotaddition/infomanager/nbtleftammo/ItemLeftAmmoAmtNbtUpdater.kt
@@ -15,25 +15,27 @@ import org.bukkit.event.Listener
 // 여기서 WeaponInfoExtractor이 Left Ammo에 관해 이 클래스에 의존성을 가지니, 여기서 WeaponInfoExtractor의 Left Ammo에 관한 메소드는 쓰면 안됨
 object ItemLeftAmmoAmtNbtUpdater: Listener {
 
-    // NBT를 처음 초기화해주는 목적으로 넣었음. (먼저 초기화 안해주면, 밑의 -1, +1에서 문제여부가 있기에) //
+    // NBT를 처음 초기화해주는 목적으로 넣었음. (먼저 초기화를 안해주면, 다른 곳에서 getLeftSideAmmoAmt() 에서 null이 나오기 때문) //
     @EventHandler(priority = EventPriority.LOWEST)
     fun onItemHeld(e: InventoryItemChangedEvent) {
         val p = e.player
         val newItem = e.newItemStack
         if (!CrackShotAdditionAPI.isValidCrackShotWeapon(newItem)) return
 
-        val weaponInfo = WeaponInfoExtractor(p, newItem)
         val nbti = NBTItem(newItem)
-        if (weaponInfo.reloadEnabled && nbti.hasKey(LEFTSIDE_AMMO_AMT_KEY) || nbti.hasKey(RIGHTSIDE_AMMO_AMT_KEY)) return  // 무조건 NBT 없을 때만 초기화
-        ItemLeftAmmoAmtNbt.updateLeftAmmoAmtNbt(p, newItem)  // initialize left ammo nbt
+        // 무조건 NBT 없을 때만 초기화
+        if (!nbti.hasKey(LEFTSIDE_AMMO_AMT_KEY) && !nbti.hasKey(RIGHTSIDE_AMMO_AMT_KEY)) {
+            ItemLeftAmmoAmtNbt.updateLeftAmmoAmtNbt(p, newItem)  // Initialize left ammo nbt
+        }
     }
 
     // 총 쏠 때 Left Ammo NBT 변경 //
+    // TODO: AirStrike같은 일회성 아이템은 onShoot이 실행된다고 하면, 아이템이 파괴되니 오류가 발생할 것임. 즉 airStrike는 onShoot 이벤트에 걸리지 않아야 함. 또는 밑에 예외를 추가하거나
     @EventHandler
     fun onShoot(e: WeaponShootEvent) {
         val p = e.player
         val heldItem = p.inventory.itemInMainHand
-        ItemLeftAmmoAmtNbt.updateLeftAmmoAmtNbt(p, heldItem) { it.leftAmmoAmt!! - 1 }
+        ItemLeftAmmoAmtNbt.updateLeftAmmoAmtNbt(p, heldItem)
     }
 
     // 장전할 때 Left Ammo NBT 변경 //
@@ -41,12 +43,6 @@ object ItemLeftAmmoAmtNbtUpdater: Listener {
     fun onReload(e: WeaponReloadCompleteEvent) {
         val p = e.player
         val heldItem = p.inventory.itemInMainHand
-        ItemLeftAmmoAmtNbt.updateLeftAmmoAmtNbt(p, heldItem) { 
-            if (it.takeAsMagazine!!) {
-                it.reloadCapacity!!
-            } else {
-                it.leftAmmoAmt!! + 1
-            }
-        }
+        ItemLeftAmmoAmtNbt.updateLeftAmmoAmtNbt(p, heldItem)
     }
 }
