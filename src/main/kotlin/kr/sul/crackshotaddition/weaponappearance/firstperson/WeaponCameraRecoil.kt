b413e79b1e7aa15f265ce6b5d281040f87bea18c
SuL <jjw011202@naver.com>
2020-11-13 02:57:25 +0900

    CameraRecoil 코드 가독성 개선

--- src/main/kotlin/kr/sul/crackshotaddition/weaponappearance/firstperson/WeaponCameraRecoil.kt
+++ src/main/kotlin/kr/sul/crackshotaddition/weaponappearance/firstperson/WeaponCameraRecoil.kt
@@ -3,7 +3,6 @@ package kr.sul.crackshotaddition.weaponappearance.firstperson
 import com.shampaggon.crackshot.events.WeaponShootEvent
 import kr.sul.crackshotaddition.CrackShotAddition.Companion.csDirector
 import net.minecraft.server.v1_12_R1.PacketPlayOutPosition
-import net.minecraft.server.v1_12_R1.PacketPlayOutPosition.EnumPlayerTeleportFlags
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
@@ -11,7 +10,7 @@ import org.bukkit.event.Listener
 import java.util.*
 
 object WeaponCameraRecoil : Listener {
-    var set: Set<EnumPlayerTeleportFlags> = HashSet(listOf(EnumPlayerTeleportFlags.X, EnumPlayerTeleportFlags.Y, EnumPlayerTeleportFlags.Z, EnumPlayerTeleportFlags.X_ROT, EnumPlayerTeleportFlags.Y_ROT))
+    var set = HashSet(hashSetOf(*PacketPlayOutPosition.EnumPlayerTeleportFlags.values()))
 
     @EventHandler
     fun onWeaponShoot(e: WeaponShootEvent) {
@@ -23,8 +22,8 @@ object WeaponCameraRecoil : Listener {
         }
     }
 
-    private fun cameraRecoil(player: Player, f: Float, f2: Float) {
-        val packetPlayOutPosition = PacketPlayOutPosition(0.0, 0.0, 0.0, f, f2, set, 0)
+    private fun cameraRecoil(player: Player, yaw: Float, pitch: Float) {
+        val packetPlayOutPosition = PacketPlayOutPosition(0.0, 0.0, 0.0, yaw, pitch, set, 0)
         (player as CraftPlayer).handle.playerConnection.sendPacket(packetPlayOutPosition)
     }
 }
