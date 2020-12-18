d9207599039f2b8bb6633e4494b6a5eb9ee02b49
SuL <jjw011202@naver.com>
2020-12-19 05:07:59 +0900

    총기 Q키로는 드랍 방지, 하지만 인벤 -999로는 드랍 가능

--- src/main/kotlin/kr/sul/crackshotaddition/addition/CancelWeaponDrop.kt
+++ src/main/kotlin/kr/sul/crackshotaddition/addition/CancelWeaponDrop.kt
@@ -1,14 +1,37 @@
 package kr.sul.crackshotaddition.addition
 
+import kr.sul.crackshotaddition.CrackShotAddition.Companion.plugin
+import kr.sul.crackshotaddition.util.CrackShotAdditionAPI.csDirector
 import kr.sul.crackshotaddition.util.CrackShotAdditionAPI.isValidCrackShotWeapon
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.player.PlayerDropItemEvent
+import org.bukkit.metadata.FixedMetadataValue
 
 object CancelWeaponDrop: Listener {
     @EventHandler
+    fun onInventoryClick(e: InventoryClickEvent) {
+        if (e.whoClicked is Player) {
+            val p = e.whoClicked as Player
+            if (e.slot == -999 && csDirector.itemParentNode(e.cursor, p) != null) {
+                p.setMetadata("dr0p_authorised", FixedMetadataValue(plugin, true))
+                Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, {
+                    p.removeMetadata("dr0p_authorised", plugin)
+                }, 1)
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
     fun onDrop(e: PlayerDropItemEvent) {
-        // 총알 수가 Infinity 인 총을 들고 Q키 누르면 드랍되는거 방지
-        if (isValidCrackShotWeapon(e.itemDrop.itemStack) && !e.isCancelled) e.isCancelled = true
+        // 총알 수가 Infinity 인 총을 들고 Q키 누르면 드랍되는거 방지. 단, GUI -999 를 통해 아이템 드랍은 허용
+        Bukkit.broadcastMessage("has dr0p_authorised: ${e.player.hasMetadata("dr0p_authorised")}")
+        if (!e.player.hasMetadata("dr0p_authorised") && isValidCrackShotWeapon(e.itemDrop.itemStack) && !e.isCancelled) {
+            e.isCancelled = true
+        }
     }
 }
